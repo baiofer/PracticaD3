f83c4bb944ec6d7b285ee2c975c978194dc4c11c
Fernando Jarilla <fjarilla@gmail.com>
2020-03-07 21:44:42 +0100

    Final version

--- practica.js
+++ practica.js
@@ -1,17 +1,20 @@
 //Origen de datos
 const url = 'https://gist.githubusercontent.com/miguepiscy/2d431ec3bc101ef62ff8ddd0e476177f/raw/2482274db871e60195b7196c602700226bdd3a44/practica.json'
-const api = 'https://storage.cloud.google.com/practica-data-visualization/airbnb_mod.csv?folder&hl=es&organizationId'
+const api = 'https://raw.githubusercontent.com/baiofer/PracticaD3/master/airbnb_mod.csv'
 
 //Constantes de la aplicación
 const width = 1000
 const height = 600
+const madrid = []
 
 //Recogida de los datos
 d3.json(url)
+    //Datos de la practica
     .then( madrid => {
         d3.csv(api)
+            //Datos de airbnb
             .then( airbnb => {
-                console.log(airbnb)
+                parseAirbnb(airbnb)
             })
         drawMap(madrid)
     })
@@ -31,10 +34,21 @@ function drawMap(map) {
         .attr('width', width)
         .attr('height', height)
 
+    //Creo el título de la página
+    const groupTitle = svg
+        .append('g')
+    groupTitle   
+        .attr('class', 'title')
+        .attr('transform', 'translate(200, 30)')
+
+    const title = groupTitle
+        .append('text')
+    title
+        .text('PRECIOS MEDIOS POR BARRIOS DE MADRID')
+
     //Creo la proyección (escala)
     //El centro exacto del mapa a pintar lo saco con esta función
     const center = d3.geoCentroid(map)
-
     const projection = d3.geoMercator()
         .fitSize([(width / 2) - border, (height) - border], map)
         .center(center)
@@ -67,11 +81,14 @@ function drawMap(map) {
     //Al hacer click en un barrio, reduzco la opacity para que se note que está seleccionado
     subunits
         .on('click', function clickSubunits(d) {
+            //Detecto la selección del barrio (1 -> no seleccionado)
             if (d.selected === 1) d.selected = 0.3
             else d.selected = 1
+            //Pongo la opacidad en función de la selección
             d3.select(this).attr('opacity', d.selected)
+            //Si no esta seleccionado, borro la gráfica
             if (d.selected === 1) deleteGraph()
-            //else drawGraph(d)
+            //Si está seleccionado, pinto la gráfica
             else drawGraph(d.properties, svg)
         })
     
@@ -93,24 +110,29 @@ function fillColor(avgPrice, maxPrice) {
     return d3.interpolateRainbow(price / maxPrice)
 }
 
-function deleteGraph(){
-        
+function deleteGraph(){  
     d3.select('.graph').remove()
 }
 
 function drawGraph(dataIn, svg){
     //Preparo los datos
-    const data = parser(dataIn)
+    const data = dataIn.avgbedrooms
+    //const data1 = extractData(dataIn)
+    console.log(dataIn)
+
+    //Borro lo que haya previamente
+    deleteGraph()
 
     //Constantes de la función
-    const marginY = 20
+    const marginY = 150
     const marginX = 40
-    const anchoBarra = 20
-    const separacion = 2
+    const marginLeft = 80
+    const widthBar = 30
+    const separationBars = 20
+    const separationText = 5
+    const separationTextY = 10
+    const separationTextX = 85
 
-
-    
-    
     //Creo un array con los totales
     const total = []
     data.forEach( d => total.push(d.total))
@@ -118,26 +140,11 @@ function drawGraph(dataIn, svg){
     //Calculo el total máximo y mínimo
     const maxTotal = d3.max(total)
     const minTotal = d3.min(total)
-    console.log(maxTotal, minTotal)
-
-    //Creo un array con las bedrooms
-    const bedrooms = []
-    data.forEach( d => bedrooms.push(d.bedrooms))
-
-    //Calculo el bedrooms máximo y mínimo
-    const maxBedrooms = d3.max(bedrooms)
-    const minBedrooms = d3.min(bedrooms)
-    console.log(maxBedrooms, minBedrooms)
-
 
     //Creo las escalas para los ejes de la gráfica
-    const scaleY = d3.scaleLinear()
-        .domain([maxTotal, minTotal])
-        .range([(height - marginY), 0])
-
     const scaleX = d3.scaleLinear()
-        .domain([maxBedrooms, minBedrooms])
-        .range([marginX, (width / 2)])
+        .domain([minTotal, maxTotal])
+        .range([marginX, (width / 2) - marginLeft])
     
     //Creo un grupo para la gráfica
     const groupGraph = svg
@@ -145,49 +152,139 @@ function drawGraph(dataIn, svg){
         .attr('class', 'graph')
         .attr('transform', `translate(${ width / 2}, 0)`)
     
-    //Creo las barras y las añado al grupo
-    const bars = groupGraph
-        .selectAll('rect')
+    //Creo un grupo para las barras y textos
+    const groupBars = groupGraph
+        .selectAll('g')
         .data(data)
         .enter()
+        .append('g')
+    groupBars
+        .attr('class', 'groupBars')
+        .attr('transform', (d,i) => {
+            const coordY = height -  marginY - i * (widthBar + separationBars)
+            return `translate(${marginX}, ${ coordY})`
+        })
+
+    //Creo las barras y las añado al grupo groupBars
+    const bars = groupBars
         .append('rect')
     bars
         .attr('class', 'bars')
-        .attr('x', (d, i) => i * anchoBarra + separacion)
-        .attr('y', d => {
-            console.log('EjeX: ', d.total, d.total * scaleY)
-        })
-        .attr('width', anchoBarra)
-        .attr('height', d => {
-            console.log ('Eje Y: ', d.bedrooms, d.bedrooms * scaleX)
-        })
+        .attr('width', d => scaleX(d.total) - marginLeft / 2)
+        .attr('height', widthBar)
+        .attr('opacity', 0.1)
+    
+    //Creo los textos con los totales y se los añado al grupo groupBars
+    const textTotal = groupBars
+        .append('text')
+    textTotal
+        .attr('class', 'totalClass')
+        .text( d => d.total)
+        .attr('x', d => scaleX(d.total) + separationText - marginLeft / 2)
+        .attr('y', separationBars)
+        .attr('opacity', 0.5)
 
-    //Creo los ejes y los añado al grupo
-    const axisY = d3.axisLeft(scaleY)
+    //Creo los textos con el número de dormitorios y se lo añado grupo groupBars
+    const textBedrooms = groupBars
+        .append('text')
+    textBedrooms
+        .attr('class', 'bedroomClass')
+        .text( d => d.bedrooms)
+        .attr('x', - separationTextY)
+        .attr('y', separationBars)
+
+    //Creo los ejes y los añado al grupo groupGraph
+    //El eje Y no tiene escala. Lo añado a mano
+
+    const axisY = groupGraph
+        .append('line')
+    axisY
+        .attr('class', 'axisY')
+        .attr('x1', 0 + marginX)
+        .attr('y1', height - marginY + widthBar)
+        .attr('x2', 0+ marginX)
+        .attr('y2', height -  marginY -  5 * (widthBar + separationBars))
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1)
+    
+    const totalTicks = () => {
+        if (maxTotal > 20) ticks = 10
+        else ticks = maxTotal
+        return ticks
+    }
+
+    const axisX = d3.axisBottom(scaleX).ticks(totalTicks())
 
     groupGraph
         .append('g')
         .attr('class', 'axisX')
-        .attr('transform', `translate(${ marginX }, ${ marginY})`)
-        .call(axisY)
-
-    const axisX = d3.axisBottom(scaleX)
-
-    groupGraph
-        .append('g')
-        .attr('class', 'axisY')
-        .attr('transform', `translate(0, ${ height - marginY })`)
+        .attr('transform', `translate(0, ${ height - marginY + widthBar })`)
         .call(axisX)
+    
+    //Creo los textos de los ejes y los añado al grupo groupGraph
+    const textAxisX = groupGraph
+        .append('text')
+    textAxisX
+        .attr('class', 'bedroomClass')
+        .attr('transform', `translate(${ width / 4 }, ${ height - separationTextX })`)
+        .text('Total alojamientos')
+    
+    const textAxisY = groupGraph
+        .append('text')
+    textAxisY
+        .attr('class', 'bedroomClass')
+        .attr('transform', `translate(${separationBars}, ${ height -  marginY - 5 * (widthBar + separationBars) - separationTextY })`)
+        .text('Habitaciones')
+    
+    //Crear textos con nombre del barrio y precio medio
+    const textNeighbourhood = groupGraph
+        .append('text')
+    textNeighbourhood
+        .attr('class', 'neighbourhoodClass')
+        .attr('transform', 'translate(250, 200)')
+        .text(dataIn.name)
 
+    const price = dataIn.avgprice ? dataIn.avgprice : 0
+
+    const textAvgPrice = groupGraph
+        .append('text')
+    textAvgPrice
+        .attr('class', 'avgpriceClass')
+        .attr('transform', 'translate(250, 220)')
+        .text(`Precio medio: ${ price } euros`)
 }
 
-function parser(data) {
-    const avgBedrooms = data.avgbedrooms
-    console.log(avgBedrooms)
-    const arr = []
-    for (let key in avgBedrooms) {
-        arr.push(avgBedrooms[key])
+//Seleccionar alojamientos de Madrid
+function parseAirbnb(data) {
+    for (let key in data) {
+        if (data[key].City === 'Madrid') madrid.push(data[key])
     }
-    console.log(arr)
-    return arr
 }
+
+//De aquí para abajo no lo uso ya que hay un montón de barrios que no coinciden sus nombres entre los dos ficheros.
+
+//Seleccionar alojamientos del barrio seleccionado
+function extractData(data) {
+    const madridNeighbourhood = []
+    const neighbourhood = data.name
+    const barrios = []
+    console.log(neighbourhood)
+    for (let key in madrid) {
+        if (madrid[key].Neighbourhood === neighbourhood) {
+            madridNeighbourhood.push(madrid[key])
+        }
+    }
+    //console.log(madridNeighbourhood)
+    //Barrios
+    for (let key in madrid) {
+        barrios.push(madrid[key].Neighbourhood)
+    }
+    //console.log(barrios)
+    //console.log(barrios.unique())
+}
+
+//Eliminar valores repetidos de un array
+Array.prototype.unique=function(a){
+    return function(){return this.filter(a)}}(function(a,b,c){return c.indexOf(a,b+1)<0
+});
+
